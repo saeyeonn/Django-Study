e7e68e83a73fbf296d23d8490ff1aa4a7a644dd2
saeyeonn <sy01lim@naver.com>
2024-03-19 12:41:32 +0900

    Practice code of Python/class

---
+++ Python/class.py
@@ -0,0 +1,35 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def greet(self):
+        print(f"Hello, my name is {self.name}")
+
+saeyeon = Person("Saeyeon", 29)
+saeyeon.greet()
+
+cherry = Person("Cherry", 5)
+cherry.greet()
+
+
+
+
+class Flight:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+    def set_passsenger(self, name):
+        self.passengers.append(name)
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+itm_flight = Flight(500)
+print(itm_flight.capacity)
+
+people = ['Saeyeon', 'Cherry', 'Pochacco']
+
+for person in people:
+    itm_flight.set_passsenger(person)
+
+open_seats = itm_flight.open_seats()
+print(open_seats)
